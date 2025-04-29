6ccec1d973859ff7c9d7bf220a843d750c022c4b
Kauã Werle <kauawerle19@gmail.com>
2025-04-29 19:07:03 -0300

    implement ip vm

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  API_URI: 'http://13.59.81.37:8080/api/v1/',
+  API_URI: 'http://10.35.207.60:8080/api/v1/',
 };
+// export const environment_test = {
+//   API_URI: 'http://192.168.1.50:8080/api/v1/',
+// };
